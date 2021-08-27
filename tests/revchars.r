3046da549b15fb7f097347f6a87c802f3471782c
Brian Dickens <brian@hostilefork.com>
2021-08-26 23:16:39 -0400

    Add READ-CHAR Based Tests
    This gives a facility for reading one character at a time from either
    a smart terminal or a redirected input device.
    
    It is truly a *character* based interface, so it is interpreting bytes
    as UTF-8.  A separate READ-BYTE interface will be needed for reading
    one byte at a time from files.
    
    The test that's added reverses the characters of the input stream

---
+++ tests/revchars.r
@@ -0,0 +1,30 @@
+Rebol [
+    File: %revchars.r
+
+    Description: {
+        This uses the READ-CHAR interface to reverse the input stream
+        on a character-by-character basis.  This is *character* based so
+        it assumes the input is UTF-8.  Not to be confused with READ-BYTE
+        that does not do UTF-8 decoding.
+
+        Note that if a smart terminal is in use, then the ESCAPE key will
+        register as giving back a NULL...the same as end of file.
+    }
+
+    Notes: {
+        This assumes the input is a validly formed text file (newline
+        at end if not zero byte file).  To keep it validly formed, it
+        leaves the tail newline at the tail.
+    }
+]
+
+text: join text! collect [
+    until [not keep try read-char]
+]
+if not empty? text [
+    if newline <> last text [
+       fail "No newline at end of input"
+    ]
+    write-stdout next reverse text
+    write-stdout newline
+]
